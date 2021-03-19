b676d9101e5988384f96fdbb3caeaeec4cc91f9f
Kalid Carvalho Da Silva <45861620+Kalid-Carvalho@users.noreply.github.com>
2021-03-19 15:21:32 -0400

    Mudanças necessarias para funcionar o programa.

--- Curso c/alocaçãoDinamica.c
+++ Curso c/alocaçãoDinamica.c
@@ -1,27 +1,27 @@
+//programa para alocar dinamicamente tamanhos em um vetor
 #include <stdio.h>
 #include <stdlib.h>
 
-//programa para alocar dinamicamente tamanhos em um vetor
 
-//função para alocar vetor
+//declarando função para alocar vetor
 int* alocaVetor(int tamanho){
 
     int *aux;
 
-    aux = malloc (int*) (tamanho * sizeof(int));// função malloc retorna ponteiro
+    aux = (int*) malloc(tamanho * sizeof(int));// função malloc retorna ponteiro
 
-    returnaux
+    return aux;
 }
-
+// inicio do programa
 int main(){
 
     //declarando variaveis
-    int *vetor, tamanho;
+    int *vetor, tamanho;//* indica que uma variavel é um ponteiro
 
 
     //pegando tamanho do vetor que vai ser alocado
-    printf("Informe o tamanho do vetor");
-        scanf("%d", tamanho);
+    printf("Informe o tamanho do vetor: ");
+        scanf("%d", &tamanho);
     
     vetor = alocaVetor(tamanho);
 
