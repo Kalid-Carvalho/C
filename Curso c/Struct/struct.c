0c9794c08f218f3b1c52fec1da1721f07fea4ddb
Kalid Carvalho Da Silva <45861620+Kalid-Carvalho@users.noreply.github.com>
2021-03-19 20:34:52 -0400

    Update struct.c

--- Curso c/Struct/struct.c
+++ Curso c/Struct/struct.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include <windows.h>
 
+//função para imprimir todas as estruturas de aluno
+void imprimiDados(aluno aluno_regular)
+{
+    printf("Código: %d \n", aluno_regular.codigo);
+    printf("Nome: %s \n", aluno_regular.nome);
+    printf("Nota: %.2f \n", aluno_regular.nota);
+}
+
+// programa que cria struct e aluno e funcao que imprime nome de aluno
 int main()
 {
     // Define o valor das páginas de código UTF8 e default do Windows
@@ -11,7 +20,22 @@ int main()
     // Define codificação como sendo UTF-8
     SetConsoleOutputCP(CPAGE_UTF8);
 
-    
+    // criando variavel struct
+    struct aluno(
+        char nome[30];
+        int codigo;
+        float nota;
+    );
+
+    //criando variavel struct dealuno
+    struct aluno aluno_especial;
+
+    //inicializando valores de struct
+    aluno_especial.codigo = 1;
+    aluno_especial.nome = strcpy(aluno_especial.nome, "Kalid");
+    aluno_especial.nota = 10.00;
+
+    imprimiDados(aluno_especial);
 
     system("\npause");
     // Retorna codificação padrão do Windows
